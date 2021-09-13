4358b103da860b3aaf85bb91d6c1c9e823118d6f
NancyWachiuri <nancywachiuri@gmail.com>
2021-09-13 08:49:43 +0300

    Add functions configurarion

--- config.py
+++ config.py
@@ -2,8 +2,8 @@
 class Config:
 
    # NEWS_API_BASE_URL ='https://api.themoviedb.org/3/movie/{}?api_key={}'
-    NEWS_API_KEY = os.environ.get('f4c86580284a4e12bd792c731cfe34c4')
-    SECRET_KEY = os.environ.get('SECRET_KEY')
+    NEWS_API_KEY = ('f4c86580284a4e12bd792c731cfe34c4')
+    SECRET_KEY = ('SECRET_KEY')
 
 
 class ProdConfig(Config):
