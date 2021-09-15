9c5443b40749037ba731a25705afa7c134363c73
NancyWachiuri <nancywachiuri@gmail.com>
2021-09-15 23:30:07 +0300

    Add second class- Articles

--- app/models.py
+++ app/models.py
@@ -1,14 +1,25 @@
-class News:
+class Sources:
     '''
     Movie class to define Movie Objects
     '''
 
-    def __init__(self,source_name,title,author,description, url, poster,date):
-        self.id =source_name
+    def __init__(self,id,name,description, url, category, language, country):
+        self.id =id
+        self.name = name
+        self.description = description
+        self.url = url
+        self.category = category
+        self.language =  language
+        self.country = country
+
+class Articles:
+
+     def __init__(self,author,title,description, url, url_to_image, publishes_at,content):
+        self.author = author
         self.news_title = title
-        self.overview = author
-        self.news_description = description
-        self.link = url
-        self.poster = "https://image.tmdb.org/t/p/w500/" + poster
-        self.date = date
+        self.overview = description
+        self.article_url = url
+        self.image = url_to_image
+        self.publishes_at = publishes_at
+        self.content = content
         
